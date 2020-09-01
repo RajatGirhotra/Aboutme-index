52907b7f12f9aaf114469e66ba89fc9c53e5c825
RajatGirhotra <labworkrajat@gmail.com>
2020-09-01 17:17:59 +0200

    1sept

--- main.js
+++ main.js
@@ -1,31 +1,35 @@
-const dark_colors =["#056676", "#5eaaa8", "#a3d2ca", "#e8ded2", "#faf3dd", "#c8d5b9", "#8fc0a9", "#68b0ab"];
+// const dark_colors =["#056676", "#5eaaa8", "#a3d2ca", "#e8ded2", "#faf3dd", "#c8d5b9", "#8fc0a9", "#68b0ab"];
 
-const dark_fonts = ["black"];
+// const dark_fonts = ["black"];
 
 
 
 const btn = document.getElementById('btn');
+const headBtn = document.getElementById('headBtn');
 const book = document.getElementById('book');
-const color = document.querySelector(".color");
-
+// const color = document.querySelector(".color");
 
 btn.addEventListener('click', function(){
 
-  document.body.style.backgroundColor = dark_colors[Math.floor(Math.random() * dark_colors.length)];
-  document.body.style.color = dark_fonts[Math.floor(Math.random() * dark_fonts.length)];
-  document.linkColor = "red";
+  if(document.body.style.backgroundColor == "white")
+  {
+  document.body.style.backgroundColor = "#464a47" ;
+  document.body.style.color = "white";
+  headBtn.style.backgroundColor = "black";
+  }
+  else
+  {
+  document.body.style.backgroundColor = "white" ;
+  document.body.style.color = "black";
+  ribbonVertical.style.backgroundColor = "white";
+
+  }
+  
   
   
 })
 
-function change(){
-  document.body.style.boxShadow = "white";
-}
 
-
-function submitButtonStyle(_this) {
-  btn.style.backgroundColor = "red";
-}
 
 // book.addEventListener('click', function(){
 
